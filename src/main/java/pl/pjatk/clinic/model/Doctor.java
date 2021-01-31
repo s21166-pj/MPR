72c41f0a5a937693457fc68386a135b098e4c9df
Pawel Badysiak <s21166@pjwstk.edu.pl>
2021-01-31 08:15:49 +0100

    added another model called Doctor

---
+++ src/main/java/pl/pjatk/clinic/model/Doctor.java
@@ -0,0 +1,48 @@
+package pl.pjatk.clinic.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String surname;
+
+    public Doctor() {
+    }
+
+    public Doctor(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
