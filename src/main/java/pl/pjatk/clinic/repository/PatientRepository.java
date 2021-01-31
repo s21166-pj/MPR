c594cfbbccb2f1c91de148287c2f657e4748ddaf
Pawel Badysiak <s21166@pjwstk.edu.pl>
2021-01-31 13:18:04 +0100

    added searching by pesel to patient

--- src/main/java/pl/pjatk/clinic/repository/PatientRepository.java
+++ src/main/java/pl/pjatk/clinic/repository/PatientRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.pjatk.clinic.model.Patient;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
+    Optional<Patient> findPatientByPesel (String pesel);
 }
