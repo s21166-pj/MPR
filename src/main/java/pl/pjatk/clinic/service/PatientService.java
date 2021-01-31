6f3863fe8fbfe33541ab1fbcce2a19b1bba721c6
Pawel Badysiak <s21166@pjwstk.edu.pl>
2021-01-31 17:42:38 +0100

    added if that checks if patient with given PESEL already exists in db

--- src/main/java/pl/pjatk/clinic/service/PatientService.java
+++ src/main/java/pl/pjatk/clinic/service/PatientService.java
@@ -1,5 +1,6 @@
 package pl.pjatk.clinic.service;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.pjatk.clinic.exception.PeselException;
@@ -22,16 +23,17 @@ public class PatientService {
         return patientRepository.findAll();
     }
 
-    public Optional<Patient> findByPesel (String pesel) {
+    public Optional<Patient> findByPesel(String pesel) {
         return patientRepository.findPatientByPesel(pesel);
     }
 
-    public Patient update (String pesel, Patient updatedPatient) {
+    public Patient update(String pesel, Patient updatedPatient) {
         Optional<Patient> patientOptional = patientRepository.findPatientByPesel(pesel);
         if (patientOptional.isPresent()) {
             Patient patient = patientOptional.get();
             patient.setName(updatedPatient.getName());
             patient.setSurname(updatedPatient.getSurname());
+            //To change, pesel shouldn't be something u can change
             patient.setPesel(updatedPatient.getPesel());
             return patientRepository.save(patient);
         } else {
@@ -40,12 +42,16 @@ public class PatientService {
     }
 
     public Patient save(Patient patient) throws PeselException {
+        //checks name, surname and pesel for validation
+        //also prevents from saving patient with the same pesel no.
         if (!NameValidator.isValidName(patient.getName())) {
             throw new IllegalArgumentException("Name can consist of only letters or is empty");
         } else if (!NameValidator.isValidName(patient.getSurname())) {
             throw new IllegalArgumentException("Surname can consist of only letters or is empty");
-        }else if (!checkPesel(patient.getPesel())) {
+        } else if (!checkPesel(patient.getPesel())) {
             throw new PeselException();
+        } else if (patientRepository.findPatientByPesel(patient.getPesel()).isPresent()) {
+            throw new IllegalArgumentException("Patient with this pesel already exists");
         } else {
             return patientRepository.save(patient);
         }
@@ -60,12 +66,12 @@ public class PatientService {
 
             controlSum += Integer.parseInt(pesel.substring(i, i + 1)) * controlWeight[i];
 
-        int controllDigit = Integer.parseInt(pesel.substring(10, 11));
+        int controlDigit = Integer.parseInt(pesel.substring(10, 11));
 
         controlSum %= 10;
         controlSum = 10 - controlSum;
         controlSum %= 10;
 
-        return (controlSum == controllDigit);
+        return (controlSum == controlDigit);
     }
 }
